cdbdba458ac31426c8737038b6a9a86f67a976c7
Muhammad Owais <70375583+Muhammad-Owais57@users.noreply.github.com>
2025-08-21 13:17:34 +0500

    Add files via upload

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Calculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtExpression1 = new System.Windows.Forms.TextBox();
+            this.lblTop = new System.Windows.Forms.Label();
+            this.txtExpression2 = new System.Windows.Forms.TextBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.cmbOperator = new System.Windows.Forms.ComboBox();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDevide = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtExpression1
+            // 
+            this.txtExpression1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtExpression1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtExpression1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtExpression1.Location = new System.Drawing.Point(0, 53);
+            this.txtExpression1.Multiline = true;
+            this.txtExpression1.Name = "txtExpression1";
+            this.txtExpression1.Size = new System.Drawing.Size(460, 41);
+            this.txtExpression1.TabIndex = 0;
+            this.txtExpression1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtExpression1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtExpression1_KeyPress);
+            // 
+            // lblTop
+            // 
+            this.lblTop.BackColor = System.Drawing.Color.Black;
+            this.lblTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTop.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.lblTop.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTop.ForeColor = System.Drawing.Color.White;
+            this.lblTop.Location = new System.Drawing.Point(0, 0);
+            this.lblTop.Name = "lblTop";
+            this.lblTop.Size = new System.Drawing.Size(460, 40);
+            this.lblTop.TabIndex = 1;
+            this.lblTop.Text = "Calculator";
+            this.lblTop.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtExpression2
+            // 
+            this.txtExpression2.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtExpression2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtExpression2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtExpression2.Location = new System.Drawing.Point(0, 121);
+            this.txtExpression2.Multiline = true;
+            this.txtExpression2.Name = "txtExpression2";
+            this.txtExpression2.Size = new System.Drawing.Size(460, 41);
+            this.txtExpression2.TabIndex = 1;
+            this.txtExpression2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtExpression2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtExpression2_KeyPress);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.BackColor = System.Drawing.Color.White;
+            this.btnPlus.Font = new System.Drawing.Font("Microsoft Uighur", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlus.Location = new System.Drawing.Point(58, 293);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(345, 29);
+            this.btnPlus.TabIndex = 3;
+            this.btnPlus.Text = "ADD";
+            this.btnPlus.UseVisualStyleBackColor = false;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // cmbOperator
+            // 
+            this.cmbOperator.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbOperator.FormattingEnabled = true;
+            this.cmbOperator.ItemHeight = 25;
+            this.cmbOperator.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.cmbOperator.Location = new System.Drawing.Point(0, 185);
+            this.cmbOperator.Name = "cmbOperator";
+            this.cmbOperator.Size = new System.Drawing.Size(460, 33);
+            this.cmbOperator.TabIndex = 2;
+            this.cmbOperator.SelectedIndexChanged += new System.EventHandler(this.cmbOperator_SelectedIndexChanged);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.BackColor = System.Drawing.Color.White;
+            this.btnMinus.Font = new System.Drawing.Font("Microsoft Uighur", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinus.Location = new System.Drawing.Point(58, 340);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(345, 29);
+            this.btnMinus.TabIndex = 9;
+            this.btnMinus.Text = "MINUS";
+            this.btnMinus.UseVisualStyleBackColor = false;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.BackColor = System.Drawing.Color.White;
+            this.btnMultiply.Font = new System.Drawing.Font("Microsoft Uighur", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiply.Location = new System.Drawing.Point(58, 386);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(345, 29);
+            this.btnMultiply.TabIndex = 10;
+            this.btnMultiply.Text = "MULTIPLY";
+            this.btnMultiply.UseVisualStyleBackColor = false;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDevide
+            // 
+            this.btnDevide.BackColor = System.Drawing.Color.White;
+            this.btnDevide.Font = new System.Drawing.Font("Microsoft Uighur", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDevide.Location = new System.Drawing.Point(58, 435);
+            this.btnDevide.Name = "btnDevide";
+            this.btnDevide.Size = new System.Drawing.Size(345, 29);
+            this.btnDevide.TabIndex = 11;
+            this.btnDevide.Text = "DEVIDE";
+            this.btnDevide.UseVisualStyleBackColor = false;
+            this.btnDevide.Click += new System.EventHandler(this.btnDevide_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtResult.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtResult.Enabled = false;
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(0, 235);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(460, 41);
+            this.txtResult.TabIndex = 12;
+            this.txtResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtResult.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtResult_KeyPress);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(460, 494);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnDevide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.cmbOperator);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.txtExpression2);
+            this.Controls.Add(this.lblTop);
+            this.Controls.Add(this.txtExpression1);
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtExpression1;
+        private System.Windows.Forms.Label lblTop;
+        private System.Windows.Forms.TextBox txtExpression2;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.ComboBox cmbOperator;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDevide;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void txtExpression1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                if (e.KeyChar < '0' || e.KeyChar > '9')
+                {
+                    e.Handled = true;
+                }
+                if (e.KeyChar == 8)
+                {
+                    e.Handled = false;
+                }
+                if (e.KeyChar < 'A' || e.KeyChar > 'z')
+                {
+                    e.Handled = false;
+                }
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        private void txtExpression2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                if (e.KeyChar < '0' || e.KeyChar > '9')
+                {
+                    e.Handled = true;
+                }
+                if (e.KeyChar == 8)
+                {
+                    e.Handled = false;
+                }
+                if (e.KeyChar < 'A' || e.KeyChar > 'z')
+                {
+                    e.Handled = false;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void cmbOperator_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbOperator.Text == "+")
+            {
+                btnMinus.Visible = false;
+                btnMultiply.Visible = false;
+                btnDevide.Visible = false;
+            }
+            else if (cmbOperator.Text == "-")
+            {
+                btnPlus.Visible = false;
+                btnMultiply.Visible = false;
+                btnDevide.Visible = false;
+            }
+            else if (cmbOperator.Text == "*")
+            {
+                btnPlus.Visible = false;
+                btnMinus.Visible = false;
+                btnDevide.Visible = false;
+            }
+            else if (cmbOperator.Text == "/")
+            {
+                btnPlus.Visible = false;
+                btnMinus.Visible = false;
+                btnMultiply.Visible = false;
+            }
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            double Answer = 0;
+            Answer = Convert.ToDouble(txtExpression1.Text) + Convert.ToDouble(txtExpression2.Text);
+            txtResult.Text += +Answer;
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            double Answer = 0;
+            Answer = Convert.ToDouble(txtExpression1.Text) - Convert.ToDouble(txtExpression2.Text);
+            txtResult.Text += +Answer;
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            double Answer;
+            Answer = Convert.ToDouble(txtExpression1.Text) * Convert.ToDouble(txtExpression2.Text);
+            txtResult.Text = "" + Answer;
+        }
+
+        private void btnDevide_Click(object sender, EventArgs e)
+        {
+            double Answer;
+            Answer = Convert.ToDouble(txtExpression1.Text) / Convert.ToDouble(txtExpression2.Text);
+            txtResult.Text = "" + Answer;
+        }
+
+        private void txtResult_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar < '0' & e.KeyChar > '9' & e.KeyChar < 'A' & e.KeyChar > 'z')
+            {
+                e.Handled = false;
+            }
+
+        }
+
+
+    }
+}
